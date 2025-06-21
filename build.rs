91d3124d7ce30be8f1ee4e6c63eb5d0da434b9d8
hyperpastel <vmutze04@gmail.com>
2025-06-21 23:55:47 +0200

    Shell completion for more shells

--- build.rs
+++ build.rs
@@ -1,18 +1,26 @@
-use clap_complete::{generate_to, shells::Zsh};
+use clap_complete::{Shell, generate_to};
 
 include!("src/command.rs");
 
+const SHELLS: [Shell; 5] = [
+    Shell::Bash,
+    Shell::Zsh,
+    Shell::Fish,
+    Shell::PowerShell,
+    Shell::Elvish,
+];
+
 fn main() -> Result<(), std::io::Error> {
-    let outdir = match std::env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
+    let Some(outdir) = std::env::var_os("OUT_DIR") else {
+        return Ok(());
     };
 
     let mut cmd = build_cli();
-    // TODO Support multiple shells
-    let path = generate_to(Zsh, &mut cmd, "stattrack", outdir)?;
-
-    println!("cargo:warning=Zsh completion generated at {path:?}");
+    let mut path;
+    for shell in SHELLS {
+        path = generate_to(shell, &mut cmd, "stattrack", &outdir)?;
+        println!("cargo:warning={shell} completion generated at {path:?}");
+    }
 
     Ok(())
 }
